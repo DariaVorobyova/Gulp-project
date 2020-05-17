3723d09a1f5dd742b3f0fc9342622fc8539c5a48
Dasha1998 <dasha090498@gmail.com>
2020-05-17 15:12:53 +0300

    gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,10 @@ let gulp = require('gulp'),
 
 gulp.task('scss', function(){
     return gulp.src('app/scss/**/*.scss')
-    .pipe(sass())
+    .pipe(sass({outputStyle:"expanded"}))
     .pipe(gulp.dest('app/css'))
+})
+
+gulp.task('watch', function(){
+    gulp.watch('app/scss/**/*.scss', gulp.parallel('scss'))
 })
