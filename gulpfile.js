bda459355bd5394d33aee6ed33da5929a5b9e83d
Dasha1998 <dasha090498@gmail.com>
2020-05-17 11:18:59 +0300

    scss gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,9 @@
-let gulp = require('gulp');
+let gulp = require('gulp'),
+    sass = require('gulp-sass');
+
+
+gulp.task('scss', function(){
+    return gulp.src('app/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
+})
