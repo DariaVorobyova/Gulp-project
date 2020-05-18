30de818b7fdcb7a6d627de27455fc83a30540a41
Dasha1998 <dasha090498@gmail.com>
2020-05-18 09:53:54 +0300

    gulp plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,19 @@
 let gulp = require('gulp'),
     sass = require('gulp-sass'),
-    browserSync = require('browser-sync')
+    browserSync = require('browser-sync'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat')
 
 
 gulp.task('scss', function(){
     return gulp.src('app/scss/**/*.scss')
     .pipe(sass({outputStyle:"expanded"}))
     .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({stream:true}))
+})
+
+gulp.task('html', function(){
+    return gulp.src('app/*.html')
     .pipe(browserSync.reload({stream:true}))
 })
 
@@ -20,6 +27,7 @@ gulp.task('browser-sync', function() {
 
 gulp.task('watch', function(){
     gulp.watch('app/scss/**/*.scss', gulp.parallel('scss'))
+    gulp.watch('app/scss/*.html', gulp.parallel('html'))
 });
 
 gulp.task('default', gulp.parallel('browser-sync', 'watch'))
