64e807facefe6be76e6e01cbc3213be499dc622c
Dasha1998 <dasha090498@gmail.com>
2020-05-18 17:45:38 +0300

    gulp-rename

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,25 @@ let gulp = require('gulp'),
     sass = require('gulp-sass'),
     browserSync = require('browser-sync'),
     uglify = require('gulp-uglify'),
-    concat = require('gulp-concat')
+    concat = require('gulp-concat'),
+    rename = require('gulp-rename')
 
 
 gulp.task('scss', function(){
     return gulp.src('app/scss/**/*.scss')
-    .pipe(sass({outputStyle:"expanded"}))
+    .pipe(sass({outputStyle:"compressed"}))
+    .pipe(rename({suffix:'.min'}))
     .pipe(gulp.dest('app/css'))
     .pipe(browserSync.reload({stream:true}))
 })
 
 gulp.task('html', function(){
     return gulp.src('app/*.html')
+    .pipe(browserSync.reload({stream:true}))
+})
+
+gulp.task('script', function(){
+    return gulp.src('app/js/*.js')
     .pipe(browserSync.reload({stream:true}))
 })
 
@@ -39,7 +46,7 @@ gulp.task('browser-sync', function() {
 gulp.task('watch', function(){
     gulp.watch('app/scss/**/*.scss', gulp.parallel('scss'))
     gulp.watch('app/scss/*.html', gulp.parallel('html'))
-    gulp.watch('app/js/*.js', gulp.parallel('js'))
+    gulp.watch('app/js/*.js', gulp.parallel('script'))
 });
 
-gulp.task('default', gulp.parallel('browser-sync', 'watch'))
+gulp.task('default', gulp.parallel('scss', 'js', 'browser-sync', 'watch'))
