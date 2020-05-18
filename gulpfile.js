a0da738b393f65883d9acbd84d210ce4bef7b2dd
Dasha1998 <dasha090498@gmail.com>
2020-05-18 15:54:07 +0300

    js task

--- gulpfile.js
+++ gulpfile.js
@@ -39,6 +39,7 @@ gulp.task('browser-sync', function() {
 gulp.task('watch', function(){
     gulp.watch('app/scss/**/*.scss', gulp.parallel('scss'))
     gulp.watch('app/scss/*.html', gulp.parallel('html'))
+    gulp.watch('app/js/*.js', gulp.parallel('js'))
 });
 
 gulp.task('default', gulp.parallel('browser-sync', 'watch'))
